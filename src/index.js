647a1ef4b5f32686f5ccb3a9a8983548204553d5
jhonSpr <jhonreyes20h@gmail.com>
2022-08-26 16:45:33 -0500

    a

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import AlertifyTest from "./alertifyTest";
 // import { Animes7 } from './Componentes/Componente7';
 // import { Animes6 } from './Componentes/componente6';
 import { Animes5 } from './Componentes/Componente5';
+import { LoginButton } from './login';
 // import Search from './searchComponent';
 
 
@@ -48,6 +49,7 @@ root.render(
   <App/>
   <Videos/>
   <Animes5/>
+  <LoginButton/>
   </Auth0Provider>
   
   
