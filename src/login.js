a81a2d25e8e05b87d03121b164d885db5c4ff4aa
jhonSpr <jhonreyes20h@gmail.com>
2022-08-26 16:41:28 -0500

    a

--- src/login.js
+++ src/login.js
@@ -4,5 +4,5 @@ import useAuth0 from "@auth0/auth0-react/dist/use-auth0";
 export const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Iniciar Sesion</button>
+    return <button onClick={() => loginWithRedirect()} className="Boton_Login">Iniciar Sesion</button>
 }
