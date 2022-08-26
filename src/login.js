ce6199dd89fdc9613ecb281c1a576fdae1722645
jhonSpr <jhonreyes20h@gmail.com>
2022-08-26 17:04:46 -0500

    a

--- src/login.js
+++ src/login.js
@@ -4,6 +4,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
   
-    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+    return <button onClick={() => loginWithRedirect()} className="button is-orange">Log In</button>;
   };
   
