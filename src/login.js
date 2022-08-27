d99fba3a8d96066747f38e1c7a421b014e3449e5
jhonSpr <jhonreyes20h@gmail.com>
2022-08-27 17:24:02 -0500

    a

--- src/login.js
+++ src/login.js
@@ -8,7 +8,6 @@ export const LoginButton = () => {
     return (
         <li className="navbar-item item-nav">
             <button onClick={() => loginWithRedirect()} className="button is-orange">Iniciar Sesion</button>
-            <a href="https://dev-xb-8m80m.us.auth0.com/u/signup?state=hKFo2SAteXRJVURiNVZMQXVnZ3pJOG5Eb25mNktWUEUySnlLa6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIFdONDVWWjZCQlhTc1pYZG80ZTB2MmkxZTA3U1lkbmN5o2NpZNkgM2JhZUdveG55NzE5MHVGbDZDUWZuN2I0NnRraVdhWHk" className="button is-orange boton-registrarse">Registrarse</a>
         </li>
         
 
