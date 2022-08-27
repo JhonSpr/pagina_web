92b5211300d036fc3dcbec1761432156a2a67f4d
jhonSpr <jhonreyes20h@gmail.com>
2022-08-27 17:29:31 -0500

    a

--- src/perfil.js
+++ src/perfil.js
@@ -19,7 +19,7 @@ const Profile = () => {
        
         <a href="/perfil"> 
         
-        <h2 className="nickname">{user.nickname}</h2>
+        <h2 className="nickname">{user.name}</h2>
        
       </a>
         
