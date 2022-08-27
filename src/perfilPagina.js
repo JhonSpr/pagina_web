40cf95d48077a1d5b76bcaa1bd86fcd46010bc96
jhonSpr <jhonreyes20h@gmail.com>
2022-08-26 20:10:16 -0500

    a

--- src/perfilPagina.js
+++ src/perfilPagina.js
@@ -8,8 +8,8 @@ const PerfilPag = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div class="spinner-grow" style={"width: 3rem; height: 3rem;"} role="status">
-    <span class="visually-hidden">Loading...</span>
+    return <div className="spinner-grow" role="status">
+    <span className="visually-hidden">Loading...</span>
   </div>;
   }
 
