f7c34de0a7e1f1df9c88f0c7f7a48dced0ea4ba9
jhonSpr <jhonreyes20h@gmail.com>
2022-08-26 21:08:10 -0500

    a

--- src/perfilPagina.js
+++ src/perfilPagina.js
@@ -20,7 +20,7 @@ const PerfilPag = () => {
       <div className="contenedor-informacion container">
        
        <div className="container-imagen">
-       <img src={user.picture} alt={user.name}/>
+       <img src={user.picture} alt={user.name} className="imagen-perfil"/>
        </div>
        <div className="container-informacion">
        <ul>
