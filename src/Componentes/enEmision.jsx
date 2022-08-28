95f7df180c38b32d7d13bf250b60284e055f8004
jhonSpr <jhonreyes20h@gmail.com>
2022-08-28 11:07:19 -0500

    a

--- src/Componentes/enEmision.jsx
+++ src/Componentes/enEmision.jsx
@@ -67,7 +67,7 @@ export function EnEmision({year1,year2,year3,year4,year5,year6,year7,year8,year9
       <div className="dropdown filter-item">
   <div className="dropdown-toggle"  data-bs-toggle="dropdown" aria-expanded="false">
   </div>
-  <ul className="dropdown-menu dropdown-menu-dark">
+  <ul className="menu-drop">
     <li>{isAuthenticated ? <> 
                                     
                                     <LogoutButton/>
