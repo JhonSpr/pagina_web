a19943c3393dcbcf6a2e1262116a336c5293123d
jhonSpr <jhonreyes20h@gmail.com>
2022-08-27 22:30:46 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -61,13 +61,7 @@ export function Inicio({href1,href2,href3,href4,href5,href6,href7,href8,href9,hr
                            <li className='navbar-item item-nav' >
                                <a className='nav-link item-nav' href='/ovas'>Ovas</a>
                            </li>
-                         
-                          
-       
-                          
-      </ul>
-      <form class="d-flex" role="search">
-      <Profile/>
+                           <Profile/>
            
            {isAuthenticated ? <> 
                                             
@@ -76,6 +70,12 @@ export function Inicio({href1,href2,href3,href4,href5,href6,href7,href8,href9,hr
                                             : <LoginButton/> 
                                              
                                             }
+                          
+       
+                          
+      </ul>
+      <form class="d-flex" role="search">
+    
       <SearchBar placeholder={"Buscar animes..."} data={BookData} imagenSearch={img}/>
       </form>
     </div>
