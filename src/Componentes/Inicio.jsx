4cf7e152f178120a8ea82188669f84af28ceb8af
jhonSpr <jhonreyes20h@gmail.com>
2022-08-28 03:49:00 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -63,13 +63,15 @@ export function Inicio({href1,href2,href3,href4,href5,href6,href7,href8,href9,hr
                            </li>
                            <Profile/>
            
-           {isAuthenticated ? <> 
+          <div className="dropdown-menu">
+          {isAuthenticated ? <> 
                                             
                                             <LogoutButton/>
                                             </>
                                             : <LoginButton/> 
                                              
                                             }
+          </div>
                          
       </ul>
     
