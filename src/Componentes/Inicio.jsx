34e86d11fd54052eee3996158888ef4ba0dabce6
jhonSpr <jhonreyes20h@gmail.com>
2022-08-27 22:09:55 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -46,15 +46,34 @@ export function Inicio({href1,href2,href3,href4,href5,href6,href7,href8,href9,hr
                 </div>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+      <Profile/>
+           
+           {isAuthenticated ? <> 
+                                            
+                                            <LogoutButton/>
+                                            </>
+                                            : <LoginButton/> 
+                                             
+                                            }
+                           <li className='navbar-item mobile item-nav'>
+                               <a className='nav-link item-nav' href='/'>Inicio </a>
+                           </li>
+                           <li className='navbar-item item-nav'>
+                               <a className='nav-link item-nav' href='/animes'>Animes </a>
+                           </li>
+                           <li className='navbar-item item-nav' >
+                               <a className='nav-link item-nav' href='/en-emision'>En Emision</a>
+                           </li>
+                         
+                           <Alertas/>
+                           <li className='navbar-item item-nav' >
+                               <a className='nav-link item-nav' href='/ovas'>Ovas</a>
+                           </li>
+                         
+                          
+                           <SearchBar placeholder={"Buscar animes..."} data={BookData} imagenSearch={img}/>
+       
+                          
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
