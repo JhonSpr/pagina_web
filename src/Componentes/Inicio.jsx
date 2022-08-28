94760f5eae162c71322d2515ad80b5d8fdeaa4dc
jhonSpr <jhonreyes20h@gmail.com>
2022-08-27 22:49:05 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -61,15 +61,7 @@ export function Inicio({href1,href2,href3,href4,href5,href6,href7,href8,href9,hr
                            <li className='navbar-item item-nav' >
                                <a className='nav-link item-nav' href='/ovas'>Ovas</a>
                            </li>
-                           <Profile/>
-           
-           {isAuthenticated ? <> 
-                                            
-                                            <LogoutButton/>
-                                            </>
-                                            : <LoginButton/> 
-                                             
-                                            }
+                         
                           
        
                           
