e1bab8f16b54a4390131a010760cbfafa5ba8daf
jhonSpr <jhonreyes20h@gmail.com>
2022-08-28 23:45:38 -0500

    a

---
+++ src/Componentes/ListaCapitulos.jsx
@@ -0,0 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import { NumerosDoce } from "./12capitulos";
+
+
+<Routes>
+    <Route path="/golden-time" element={<NumerosDoce anime={"golden Time"}/>}/>
+</Routes>
