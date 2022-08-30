18104bfcc491839e8be9b9c0b8de53e239bffb6a
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 18:07:48 -0500

    a

--- src/Componentes/Componente3.js
+++ src/Componentes/Componente3.js
@@ -88,9 +88,7 @@ export function Video({
                   </div>
                 </div>
                 <div className="column is-12-mobile is-12-tablet is-12-desktop is-paddingless">
-                  <h5 className="has-text-orange has-text-weight-semibold has-text-centered is-size-6">
-                    Otros Animes
-                  </h5>
+                  <h5 className="title-otros">Otros Animes</h5>
                   <div className="columns is-mobile is-multiline is-variable is-1 otros-animes">
                     <AnimeRecommend1 />
                     <AnimeRecommend2 />
