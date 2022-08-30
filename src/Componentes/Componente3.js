aa5cc1616c6199192280e30cd279b1af830858bf
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 17:22:21 -0500

    a

--- src/Componentes/Componente3.js
+++ src/Componentes/Componente3.js
@@ -87,6 +87,16 @@ export function Video({
                   >
                     {anime}
                   </div>
+                  <div class="column is-12-mobile is-12-tablet is-12-desktop is-paddingless">
+                    <h5 class="has-text-light text-centered">Otros Animes</h5>
+                    <div class="columns is-mobile is-multiline is-variable is-1 otros-animes"></div>
+                    <AnimeRecommend1 />
+                    <AnimeRecommend2 />
+                    <AnimeRecommend3 />
+                    <AnimeRecommend4 />
+                    <AnimeRecommend5 />
+                    <AnimeRecommend6 />
+                  </div>
                 </div>
               </div>
             </div>
