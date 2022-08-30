d5ec96bd2b764c9e583f75b14b2e1fd24b8a08ca
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 17:56:06 -0500

    a

--- src/Componentes/AnimesRecommend.js
+++ src/Componentes/AnimesRecommend.js
@@ -4,107 +4,101 @@ export function AnimeRecommend1() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
 export function AnimeRecommend2() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
 export function AnimeRecommend3() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
 export function AnimeRecommend4() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
 export function AnimeRecommend5() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
 export function AnimeRecommend6() {
   let RandomNumber = Math.floor(Math.random() * 51);
 
   return (
-    <div>
-      <a
-        href={Animes && Animes[RandomNumber].link}
-        title={Animes && Animes[RandomNumber].title}
-      >
-        <img
-          src={Animes && Animes[RandomNumber].img}
-          alt={Animes && Animes[RandomNumber].title}
-          className="img-otros-animes"
-        />
-      </a>
-    </div>
+    <a
+      href={Animes && Animes[RandomNumber].link}
+      title={Animes && Animes[RandomNumber].title}
+      className="div-img"
+    >
+      <img
+        src={Animes && Animes[RandomNumber].img}
+        alt={Animes && Animes[RandomNumber].title}
+        className="img-otros-animes"
+      />
+    </a>
   );
 }
