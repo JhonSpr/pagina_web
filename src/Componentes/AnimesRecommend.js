9bf8e098e3188c5f428c2bf5e4f519cbd694516f
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 16:52:43 -0500

    a

--- src/Componentes/AnimesRecommend.js
+++ src/Componentes/AnimesRecommend.js
@@ -2,6 +2,11 @@ import Animes from "../Data.json";
 
 export function AnimesRecommend() {
   let RandomNumber = Math.floor(Math.random() * 100);
+  let RandomNumber2 = Math.floor(Math.random() * 100);
+  let RandomNumber3 = Math.floor(Math.random() * 100);
+  let RandomNumber4 = Math.floor(Math.random() * 100);
+  let RandomNumber5 = Math.floor(Math.random() * 100);
+  let RandomNumber6 = Math.floor(Math.random() * 100);
   return (
     <div>
       <div class="column is-12-mobile is-12-tablet is-12-desktop is-paddingless">
@@ -21,60 +26,60 @@ export function AnimesRecommend() {
           </div>
           <div class="column is-narrow is-3-mobile is-6-tablet is-6-desktop otros-animes-sub-div">
             <a
-              href={Animes && Animes[RandomNumber].link}
-              title={Animes && Animes[RandomNumber].title}
+              href={Animes && Animes[RandomNumber2].link}
+              title={Animes && Animes[RandomNumber2].title}
             >
               <img
-                src={Animes && Animes[RandomNumber].img}
-                alt={Animes && Animes[RandomNumber].title}
+                src={Animes && Animes[RandomNumber2].img}
+                alt={Animes && Animes[RandomNumber2].title}
                 className="img-otros-animes"
               />
             </a>
           </div>
           <div class="column is-narrow is-3-mobile is-6-tablet is-6-desktop otros-animes-sub-div">
             <a
-              href={Animes && Animes[RandomNumber].link}
-              title={Animes && Animes[RandomNumber].title}
+              href={Animes && Animes[RandomNumber3].link}
+              title={Animes && Animes[RandomNumber3].title}
             >
               <img
-                src={Animes && Animes[RandomNumber].img}
-                alt={Animes && Animes[RandomNumber].title}
+                src={Animes && Animes[RandomNumber3].img}
+                alt={Animes && Animes[RandomNumber3].title}
                 className="img-otros-animes"
               />
             </a>
           </div>
           <div class="column is-narrow is-3-mobile is-6-tablet is-6-desktop otros-animes-sub-div">
             <a
-              href={Animes && Animes[RandomNumber].link}
-              title={Animes && Animes[RandomNumber].title}
+              href={Animes && Animes[RandomNumber4].link}
+              title={Animes && Animes[RandomNumber4].title}
             >
               <img
-                src={Animes && Animes[RandomNumber].img}
-                alt={Animes && Animes[RandomNumber].title}
+                src={Animes && Animes[RandomNumber4].img}
+                alt={Animes && Animes[RandomNumber4].title}
                 className="img-otros-animes"
               />
             </a>
           </div>
           <div class="column is-narrow is-3-mobile is-6-tablet is-6-desktop otros-animes-sub-div">
             <a
-              href={Animes && Animes[RandomNumber].link}
-              title={Animes && Animes[RandomNumber].title}
+              href={Animes && Animes[RandomNumber5].link}
+              title={Animes && Animes[RandomNumber5].title}
             >
               <img
-                src={Animes && Animes[RandomNumber].img}
-                alt={Animes && Animes[RandomNumber].title}
+                src={Animes && Animes[RandomNumber5].img}
+                alt={Animes && Animes[RandomNumber5].title}
                 className="img-otros-animes"
               />
             </a>
           </div>
           <div class="column is-narrow is-3-mobile is-6-tablet is-6-desktop otros-animes-sub-div">
             <a
-              href={Animes && Animes[RandomNumber].link}
-              title={Animes && Animes[RandomNumber].title}
+              href={Animes && Animes[RandomNumber6].link}
+              title={Animes && Animes[RandomNumber6].title}
             >
               <img
-                src={Animes && Animes[RandomNumber].img}
-                alt={Animes && Animes[RandomNumber].title}
+                src={Animes && Animes[RandomNumber6].img}
+                alt={Animes && Animes[RandomNumber6].title}
                 className="img-otros-animes"
               />
             </a>
