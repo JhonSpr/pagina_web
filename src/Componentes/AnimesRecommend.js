1bf18ab389b4785685a0c86c2e19041a2eea9c46
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 16:19:46 -0500

    a

--- src/Componentes/AnimesRecommend.js
+++ src/Componentes/AnimesRecommend.js
@@ -4,7 +4,7 @@ export function AnimesRecommend() {
   const randomAnime = Math.floor(Math.random) * 100;
   return (
     <div>
-      <a href="">
+      <a href={Animes && Animes[randomAnime].link}>
         <img src={Animes && Animes[randomAnime].img} alt="" />
       </a>
     </div>
