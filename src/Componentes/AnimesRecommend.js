0dabcbe7f0af6e045953af41f7dfcea8a9df2121
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 16:02:06 -0500

    a

--- src/Componentes/AnimesRecommend.js
+++ src/Componentes/AnimesRecommend.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
 import Animes from "../Data.json";
 
 export function AnimesRecommend() {
+  Animes.slice(0, 6).map((animes) => {
+    return (
+      <div class="item" key={animes.id}>
+        <div class="overarchingdiv">
+          <a href={animes.link}>
+            <img src={animes.img} alt={animes.title} />
+            <div class="seriesoverlay has-text-orange">
+              <h3 class="has-text-centered">
+                <div class="overtitlenews has-text-weight-semibold">
+                  {animes.title}
+                </div>
+              </h3>
+            </div>
+          </a>
+        </div>
+      </div>
+    );
+  });
   Animes = (array) => {
     let randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
