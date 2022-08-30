9ef607d21ccec929d7e1505aaf008b39c7e955bb
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 18:50:28 -0500

    a

--- src/Componentes/Componente3.js
+++ src/Componentes/Componente3.js
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/style-prop-object */
 
-// import {
-//   AnimeRecommend1,
-//   AnimeRecommend2,
-//   AnimeRecommend3,
-//   AnimeRecommend4,
-// } from "./AnimesRecommend";
-// import { Disqus } from "./disqus";
-import Prueba from "./prueba";
+import {
+  AnimeRecommend1,
+  AnimeRecommend2,
+  AnimeRecommend3,
+  AnimeRecommend4,
+} from "./AnimesRecommend";
+import { Disqus } from "./disqus";
+// import Prueba from "./prueba";
 
 /* eslint-disable jsx-a11y/iframe-has-title */
 export function Video({
@@ -16,6 +16,7 @@ export function Video({
   anime,
   capitulosVideo,
   siguienteVideo,
+  anteriorVideo,
   numero,
   contAnteriorClass,
   contSiguienteClass,
@@ -23,84 +24,85 @@ export function Video({
   frame1,
 }) {
   return (
-    // <div>
-    //   <title>{tituloAnime}</title>
+    <div>
+      <title>{tituloAnime}</title>
 
-    //   <div className="hero is-fullheightX">
-    //     <section className="section">
-    //       <div className="container">
-    //         <div className="columns is-multiline">
-    //           <div className="column is-12-mobile is-8-tablet is-9-desktop">
-    //             <h1 className="title is-size-4 has-text-centeredX has-text-weight-semibold is-uppercaseX has-text-light">
-    //               {anime} {numero}
-    //             </h1>
-    //           </div>
+      <div className="hero is-fullheightX">
+        <section className="section">
+          <div className="container">
+            <div className="columns is-multiline">
+              <div className="column is-12-mobile is-8-tablet is-9-desktop">
+                <h1 className="title is-size-4 has-text-centeredX has-text-weight-semibold is-uppercaseX has-text-light">
+                  {anime} {numero}
+                </h1>
+              </div>
 
-    //           <div className="column is-12-mobile is-9-tablet is-9-desktop">
-    //             <div className="player-container">
-    //               <div className="iframe-container" id="video_player">
-    //                 <iframe
-    //                   src={frame1}
-    //                   id="div1"
-    //                   scrolling="no"
-    //                   allowFullScreen
-    //                 ></iframe>
-    //               </div>
-    //               <div className="column is-12-mobile is-4-tablet is-3-desktop">
-    //                 <div className="columns is-variable is-1 cont-opciones">
-    //                   <div className={contAnteriorClass}>
-    //                     <a
-    //                       href={anteriorVideo}
-    //                       className="button is-orange is-fullwidth"
-    //                     >
-    //                       <i className="fa fa-arrow-left"></i>&nbsp;
-    //                     </a>
-    //                   </div>
+              <div className="column is-12-mobile is-9-tablet is-9-desktop">
+                <div className="player-container">
+                  <div className="iframe-container" id="video_player">
+                    <iframe
+                      src={frame1}
+                      id="div1"
+                      scrolling="no"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                  <div className="column is-12-mobile is-4-tablet is-3-desktop">
+                    <div className="columns is-variable is-1 cont-opciones">
+                      <div className={contAnteriorClass}>
+                        <a
+                          href={anteriorVideo}
+                          className="button is-orange is-fullwidth"
+                        >
+                          <i className="fa fa-arrow-left"></i>&nbsp;
+                        </a>
+                      </div>
 
-    //                   <div className="column is-6-desktop is-8-tablet is-8-mobile cont-capitulos">
-    //                     <a href={capitulosVideo} className="button is-dark">
-    //                       &nbsp;Episodios
-    //                     </a>
-    //                   </div>
+                      <div className="column is-6-desktop is-8-tablet is-8-mobile cont-capitulos">
+                        <a href={capitulosVideo} className="button is-dark">
+                          &nbsp;Episodios
+                        </a>
+                      </div>
 
-    //                   <div className={contSiguienteClass}>
-    //                     <a
-    //                       href={siguienteVideo}
-    //                       className="button is-orange is-fullwidth"
-    //                     >
-    //                       <i className="fa fa-arrow-right"></i>&nbsp;
-    //                     </a>
-    //                   </div>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //             <h1 className="title is-size-5 has-text-weight-light has-text-light">
-    //               {tituloPag} Online HD
-    //             </h1>
-    //             <div id="commentsContainer">
-    //               <div
-    //                 id="showComments"
-    //                 className="button is-fullwidth is-dark"
-    //               >
-    //                 {anime}
-    //               </div>
-    //             </div>
-    //             <div className="column is-12-mobile is-12-tablet is-12-desktop is-paddingless contenedor-otros">
-    //               <div className="columns is-mobile is-multiline is-variable is-1 otros-animes">
-    //                 <h5 className="title-otros">Otros Animes</h5>
-    //                 <AnimeRecommend1 />
-    //                 <AnimeRecommend2 />
-    //                 <AnimeRecommend3 />
-    //                 <AnimeRecommend4 />
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </section>
-    //     <Disqus />
-    //   </div>
-    // </div>
-    <Prueba />
+                      <div className={contSiguienteClass}>
+                        <a
+                          href={siguienteVideo}
+                          className="button is-orange is-fullwidth"
+                        >
+                          <i className="fa fa-arrow-right"></i>&nbsp;
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h1 className="title is-size-5 has-text-weight-light has-text-light">
+                  {tituloPag} Online HD
+                </h1>
+                <div id="commentsContainer">
+                  <div
+                    id="showComments"
+                    className="button is-fullwidth is-dark"
+                  >
+                    {anime}
+                  </div>
+                </div>
+                <div className="column is-12-mobile is-3-tablet is-3-deskop">
+                  <div className="column is-12-mobile is-12-tablet is-12-desktop is-paddingless contenedor-otros">
+                    <div className="columns is-mobile is-multiline is-variable is-1 otros-animes">
+                      <h5 className="title-otros">Otros Animes</h5>
+                      <AnimeRecommend1 />
+                      <AnimeRecommend2 />
+                      <AnimeRecommend3 />
+                      <AnimeRecommend4 />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <Disqus />
+      </div>
+    </div>
   );
 }
