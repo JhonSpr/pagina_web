0035d44c8cad085128d4770181c2ecadc84fd44d
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 16:29:03 -0500

    a

--- src/Componentes/AnimesRecommend.js
+++ src/Componentes/AnimesRecommend.js
@@ -1,7 +1,7 @@
 import Animes from "../Data.json";
 
 export function AnimesRecommend() {
-  let RandomNumber = Math.floor(Math.random);
+  let RandomNumber = Math.floor(Math.random() * 100);
   return (
     <div>
       <a href={Animes && Animes[RandomNumber].link}>
