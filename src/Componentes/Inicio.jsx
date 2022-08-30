0fc6e6800a1aadaa9c63a90ce68d15d573f8768a
jhonSpr <jhonreyes20h@gmail.com>
2022-08-30 17:05:06 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -65,7 +65,6 @@ export function Inicio({
 
       <div className="hero is-lightx is-fullheight">
         <div>
-          <AnimesRecommend />
           <section className="section page-home__latest-series">
             <div className="container rounded-container">
               <h1 className="titulo title is-size-4 has-text-centered has-text-weight-semibold has-text-light is-uppercase">
