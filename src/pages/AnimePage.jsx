1e2e93c6a755e49dc828354bc71d39a264b79725
jhonSpr <jhonreyes20h@gmail.com>
2022-09-05 19:01:31 -0500

    a

--- src/pages/AnimePage.jsx
+++ src/pages/AnimePage.jsx
@@ -1,38 +1,7 @@
 import React from "react";
 import Animes from "../Data.json";
 
-export function AnimePage1() {
-  return Animes.slice(0, 23).map((anime) => {
-    return (
-      <article className="serie-card" title={anime.title} key={anime.id}>
-        <figure className="image overarchingdiv2">
-          <a href={anime.link}>
-            <img src={anime.img} alt={anime.title} />
-            <div className="overlay-dark"></div>
-            <div className="hoveroverlay">
-              <i className="fas fa-play pgnav activehov"></i>
-            </div>
-          </a>
-          <span className="tag year is-dark">{anime.year}</span>
-          <span className="tag is-danger type">TV</span>
-          {/* <span className={classEmision10}>{anime.}</span> */}
-          <div className="title">
-            <h3>
-              <a
-                href={anime.link}
-                className="has-text-orange has-text-weight-semibold has-text-centered is-size-6"
-              >
-                {anime.title}
-              </a>
-            </h3>
-          </div>
-        </figure>
-      </article>
-    );
-  });
-}
-
-export function AnimePage2() {
+export default function AnimePage2() {
   Animes.slice(24, 47).map((anime) => {
     return (
       <article className="serie-card" title={anime.title} key={anime.id}>
