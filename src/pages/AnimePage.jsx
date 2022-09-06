b1fa040a1a773ae605146f5717cfff07e2c4cda6
jhonSpr <jhonreyes20h@gmail.com>
2022-09-05 19:14:54 -0500

    a

--- src/pages/AnimePage.jsx
+++ src/pages/AnimePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Animes from "../Data.json";
 
 export default function AnimePage2() {
-  Animes.slice(24, 47).map((anime) => {
+  return Animes.slice(24, 47).map((anime) => {
     return (
       <article className="serie-card" title={anime.title} key={anime.id}>
         <figure className="image overarchingdiv2">
