f16f6707638fd1a5570606c98e377861c547f295
jhonSpr <jhonreyes20h@gmail.com>
2022-09-06 02:01:15 -0500

    a

--- src/pages/GeneroPage.jsx
+++ src/pages/GeneroPage.jsx
@@ -31,33 +31,3 @@ export default function GeneroPage1() {
     );
   });
 }
-export function GeneroPage2() {
-  return Animes.slice(207, 230).map((anime) => {
-    return (
-      <article className="serie-card" title={anime.title} key={anime.id}>
-        <figure className="image overarchingdiv2">
-          <a href={anime.link}>
-            <img src={anime.img} alt={anime.title} />
-            <div className="overlay-dark"></div>
-            <div className="hoveroverlay">
-              <i className="fas fa-play pgnav activehov"></i>
-            </div>
-          </a>
-          <span className="tag year is-dark">{anime.year}</span>
-          <span className="tag is-danger type">TV</span>
-          <span className={anime.classEstado}>{anime.estado}</span>
-          <div className="title">
-            <h3>
-              <a
-                href={anime.link}
-                className="has-text-orange has-text-weight-semibold has-text-centered is-size-6"
-              >
-                {anime.title}
-              </a>
-            </h3>
-          </div>
-        </figure>
-      </article>
-    );
-  });
-}
