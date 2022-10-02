60238bd687cd2806a20cbc7b9192fbd32ba4fd76
jhonSpr <jhonreyes20h@gmail.com>
2022-10-02 18:21:34 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -85,10 +85,10 @@ export function Inicio({
               <hr />
               <div className="list-series">
                 <Noticia />
-                <div className="container_comment">
-                  <h1 className="comment_text">¡DI ALGO!</h1>
-                  <Disqus className={"Comentarios"} />
-                </div>
+              </div>
+              <div className="container_comment">
+                <h1 className="comment_text">¡DI ALGO!</h1>
+                <Disqus className={"Comentarios"} />
               </div>
             </div>
           </section>
