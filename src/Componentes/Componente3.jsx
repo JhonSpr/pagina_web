89e3c8f085b3937445448094355cab569bec5ccb
jhonSpr <jhonreyes20h@gmail.com>
2022-11-15 14:05:55 -0500

    a

--- src/Componentes/Componente3.jsx
+++ src/Componentes/Componente3.jsx
@@ -98,10 +98,6 @@ export function Video({
                 </div>
               </div>
             </div>
-            <div className="container_comment">
-              <h1>Comentarios sobre {anime}</h1>
-              <Disqus className={"Comentarios"} />
-            </div>
           </div>
         </section>
       </div>
