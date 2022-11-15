fde05132966873e83ab3c4fdbf2bb9135d2dc0d7
jhonSpr <jhonreyes20h@gmail.com>
2022-11-15 13:45:52 -0500

    a

--- src/Componentes/Componente3.jsx
+++ src/Componentes/Componente3.jsx
@@ -90,12 +90,12 @@ export function Video({
                     <AnimeRecommend4 />
                   </div>
                 </div>
-              </div>
-              <div className="container_comment">
-                <h1 className="comment_text">
-                  Usa ADBLOCK para ver los capitulos
-                </h1>
-                <Disqus className={"Comentarios"} />
+                <div className="container_comment">
+                  <h1 className="comment_text">
+                    Usa ADBLOCK para ver los capitulos
+                  </h1>
+                  <Disqus className={"Comentarios"} />
+                </div>
               </div>
             </div>
           </div>
