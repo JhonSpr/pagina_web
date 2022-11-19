af9ff6d0ffbf2a1eb12d9726e6cfc7044cf7c650
jhonSpr <jhonreyes20h@gmail.com>
2022-11-18 23:40:20 -0500

    a

--- src/videos.jsx
+++ src/videos.jsx
@@ -10025,7 +10025,7 @@ function Videos() {
         path="/ver/mob-psycho-100-III-7"
         element={
           <Video
-            frame1={""}
+            frame1={"https://sbthe.com/e/vx7lug8864h4.html"}
             contAnteriorClass="column is-3-desktop is-2-tablet is-2-mobile cont-anterior"
             contSiguienteClass={"disable"}
             tituloAnime={"Mob Psycho 100 III Capitulo 7"}
