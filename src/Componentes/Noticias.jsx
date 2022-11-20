33bd1fbc28df725ef76c52ff4184227cc5dd7f3d
jhonSpr <jhonreyes20h@gmail.com>
2022-11-20 00:36:35 -0500

    a

--- src/Componentes/Noticias.jsx
+++ src/Componentes/Noticias.jsx
@@ -1,4 +1,5 @@
 import Noticias from "../Noticias.json";
+import RecienAgregados from "../Data.json";
 export function Noticia() {
   return (
     <section>
@@ -12,6 +13,31 @@ export function Noticia() {
                   <h3 class="has-text-centered">
                     <div class="overtitlenews has-text-weight-semibold">
                       {noticia.title}
+                    </div>
+                  </h3>
+                </div>
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export function Agregados() {
+  return (
+    <section>
+      {RecienAgregados.map((agregados, id) => {
+        return (
+          <div class="item" key={agregados.id}>
+            <div class="overarchingdiv">
+              <a href={agregados.link}>
+                <img src={agregados.img} alt={agregados.title} />
+                <div class="seriesoverlay has-text-orange">
+                  <h3 class="has-text-centered">
+                    <div class="overtitlenews has-text-weight-semibold">
+                      {agregados.title}
                     </div>
                   </h3>
                 </div>
