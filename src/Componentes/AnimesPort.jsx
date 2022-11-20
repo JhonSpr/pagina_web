3319fa3a46a4c1bb6cc718559fe130a74b44b016
jhonSpr <jhonreyes20h@gmail.com>
2022-11-20 16:43:47 -0500

    a

--- src/Componentes/AnimesPort.jsx
+++ src/Componentes/AnimesPort.jsx
@@ -248,7 +248,6 @@ export function Animes13({
             </div>
           </div>
           <div className="container_comment">
-            <h1 className="comment_text">¡DI ALGO!</h1>
             <Disqus className={"Comentarios"} />
           </div>
         </div>
