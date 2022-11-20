02c6456891a028fc91a486f431de3d6c2156b86c
jhonSpr <jhonreyes20h@gmail.com>
2022-11-20 00:01:50 -0500

    a

--- src/Componentes/AnimesPort.jsx
+++ src/Componentes/AnimesPort.jsx
@@ -67,7 +67,7 @@ export function Animes13({
               <br />
             </div>
 
-            <div className="column">
+            <div className="column columnas">
               <div className="columns is-multiline is-narrow">
                 <div className="column is-12-mobile is-8-tablet is-10-desktop">
                   <h1 className="title has-text-orange">{anime}</h1>
