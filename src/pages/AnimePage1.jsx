cedd78fc53742b12bdf35f28db6ed7407219753d
jhonSpr <jhonreyes20h@gmail.com>
2022-11-19 23:46:10 -0500

    a

--- src/pages/AnimePage1.jsx
+++ src/pages/AnimePage1.jsx
@@ -33,31 +33,27 @@ export default function AnimePage1() {
   });
 }
 export function Emision1() {
-  return emisiones.slice(0, 24).map((anime) => {
+  return emisiones.slice(0, 24).map((emision) => {
     return (
-      <article
-        className="serie-card"
-        title={emisiones.title}
-        key={emisiones.id}
-      >
+      <article className="serie-card" title={emision.title} key={emision.id}>
         <figure className="image overarchingdiv2">
-          <a href={emisiones.link}>
-            <img src={emisiones.img} alt={emisiones.title} />
+          <a href={emision.link}>
+            <img src={emision.img} alt={emision.title} />
             <div className="overlay-dark"></div>
             <div className="hoveroverlay">
               <i className="fas fa-play pgnav activehov"></i>
             </div>
           </a>
-          <span className="tag year is-dark">{emisiones.year}</span>
+          <span className="tag year is-dark">{emision.year}</span>
           <span className="tag is-danger type">TV</span>
-          <span className={emisiones.classEstado}>{emisiones.estado}</span>
+          <span className={emision.classEstado}>{emision.estado}</span>
           <div className="title">
             <h3>
               <a
-                href={emisiones.link}
+                href={emision.link}
                 className="has-text-orange has-text-weight-semibold has-text-centered is-size-6"
               >
-                {emisiones.title}
+                {emision.title}
               </a>
             </h3>
           </div>
