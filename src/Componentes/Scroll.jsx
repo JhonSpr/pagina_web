5bc48c9b3c4fa469e2016e16d19257d1a71b2174
jhonSpr <jhonreyes20h@gmail.com>
2022-11-30 17:13:20 -0500

    a

--- src/Componentes/Scroll.jsx
+++ src/Componentes/Scroll.jsx
@@ -20,6 +20,7 @@ export default class AutoPlay extends Component {
       pauseOnFocus: true,
       useTransform: true,
       slidesPerRow: true,
+      rows: 10,
     };
     const anime1 = "SPY x Family 2nd Temporada";
     const link1 = "/spy-x-family-2nd-temporada";
