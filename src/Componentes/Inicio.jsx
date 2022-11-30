a5e73f5ab121a453e3c25b5304060791370fdbc4
jhonSpr <jhonreyes20h@gmail.com>
2022-11-30 17:19:58 -0500

    a

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -20,7 +20,10 @@ export function Inicio() {
             <h1 className="titulo title is-size-4 has-text-centered has-text-weight-semibold has-text-light is-uppercase">
               Emisiones
             </h1>
-            <div className="container" style={{ padding: "10px" }}>
+            <div
+              className="container rounded-container"
+              style={{ padding: "10px" }}
+            >
               <PauseOnHover />
             </div>
           </section>
