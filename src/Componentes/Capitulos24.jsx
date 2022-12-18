91758286ac7daeb5ef32901f124ea5eaf452ba5d
JhonSpr <jhoreyes20h@gmail.com>
2022-12-17 19:55:21 -0500

    a

--- src/Componentes/Capitulos24.jsx
+++ src/Componentes/Capitulos24.jsx
@@ -79,7 +79,10 @@ export function Capitulos24({
       <title>{tituloAnime}</title>
 
       <section className="section">
-        <div className="image_baner"></div>
+        <div
+          className="image_baner"
+          style={{ backgroundImage: "https://i.waifu.pics/0W~KpRc.jpg" }}
+        ></div>
         <div className="container">
           <div className="columns is-mobile is-multiline">
             <div className="column is-12-mobile xis-3-tablet xis-3-desktop xhas-background-danger is-narrow-tablet is-narrow-desktop">
