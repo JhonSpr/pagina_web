7df6648ea905a0171f98cef85675c6d7446c6ab6
JhonSpr <jhoreyes20h@gmail.com>
2022-12-17 20:27:45 -0500

    a

--- src/Componentes/Elements/banner.js
+++ src/Componentes/Elements/banner.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Banner from "../../Json/Banners.json";
+import banner from "../../Json/Banners.json";
 export function Banner() {
   let end = 3;
   let RandomNumber = Math.floor(Math.random() * end);
   console.log(RandomNumber);
 
   return (
-    <img src={Banner && Banner[RandomNumber].image} className="image_baner" />
+    <img src={banner && banner[RandomNumber].image} className="image_baner" />
   );
 }
