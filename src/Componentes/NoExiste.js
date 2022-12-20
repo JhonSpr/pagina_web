528b7611cbc44bc8e12e22893268ebc68e926014
JhonSpr <jhoreyes20h@gmail.com>
2022-12-19 19:04:21 -0500

    a

--- src/Componentes/NoExiste.js
+++ src/Componentes/NoExiste.js
@@ -5,8 +5,9 @@ export function NoExiste() {
         Pagina No Encontrada <i class="fa-solid fa-triangle-exclamation"></i>{" "}
       </title>
       <h1 className="Title-NoFound">
-        <i class="fa-solid fa-triangle-exclamation"></i> Esta Pagina no
-        Existe... <i class="fa-solid fa-triangle-exclamation"></i>
+        <i class="fa-solid fa-triangle-exclamation"></i> Asegurate que el anime
+        buscado esta correctamente escrito{" "}
+        <i class="fa-solid fa-triangle-exclamation"></i>
       </h1>
       <a href="/" className="button is-orange">
         Volver Al Inicio
