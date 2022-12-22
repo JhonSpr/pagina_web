6b9f4b70069c7524d5f89e3b83ba0be7f3a8b76d
JhonSpr <jhoreyes20h@gmail.com>
2022-12-22 17:06:15 -0500

    a

--- src/Componentes/Elements/LoginMobile.js
+++ src/Componentes/Elements/LoginMobile.js
@@ -5,5 +5,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const LoginButtonMobile = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <a onClick={() => loginWithRedirect()}>Iniciar Sesion</a>;
+  return (
+    <li className="navbar-item item-nav btn-mobile">
+      <a onClick={() => loginWithRedirect()}>Iniciar Sesion</a>
+    </li>
+  );
 };
