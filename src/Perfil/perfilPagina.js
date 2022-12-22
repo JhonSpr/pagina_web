ecfdaca37a846c1aa26153738f23870fb91c4119
JhonSpr <jhoreyes20h@gmail.com>
2022-12-22 14:04:52 -0500

    a

--- src/Perfil/perfilPagina.js
+++ src/Perfil/perfilPagina.js
@@ -18,46 +18,46 @@ const PerfilPag = () => {
 
   return (
     isAuthenticated && (
-      <div className="contenedor-informacion container">
-        <div>
-          <Banner />
-        </div>
-        <div className="container-imagen">
-          <img src={user.picture} alt={user.name} className="imagen-perfil" />
-        </div>
-        <div className="container-informacion">
-          <ul>
-            <li>
-              <p className="nickname">Usuario: {user.nickname}</p>
-            </li>
-            <li>
-              <p className="nickname">Correo Electronico: {user.email}</p>
-            </li>
-            <li>
-              <p className="nickname">
-                Correo Electronico Verificado: {String(user.email_verified)}
-              </p>
-            </li>
-          </ul>
+      <>
+        <Banner />
+        <div className="contenedor-informacion container">
+          <div className="container-imagen">
+            <img src={user.picture} alt={user.name} className="imagen-perfil" />
+          </div>
+          <div className="container-informacion">
+            <ul>
+              <li>
+                <p className="nickname">Usuario: {user.nickname}</p>
+              </li>
+              <li>
+                <p className="nickname">Correo Electronico: {user.email}</p>
+              </li>
+              <li>
+                <p className="nickname">
+                  Correo Electronico Verificado: {String(user.email_verified)}
+                </p>
+              </li>
+            </ul>
 
-          <button
-            className="button is-orange"
-            type="button"
-            disabled
-            onClick={() => loginWithRedirect()}
-          >
-            Iniciar Sesion
-          </button>
-          <button
-            onClick={() => logout({ returnTo: window.location.origin })}
-            className="button is-orange"
-          >
-            Cerrar Session
-          </button>
-        </div>
+            <button
+              className="button is-orange"
+              type="button"
+              disabled
+              onClick={() => loginWithRedirect()}
+            >
+              Iniciar Sesion
+            </button>
+            <button
+              onClick={() => logout({ returnTo: window.location.origin })}
+              className="button is-orange"
+            >
+              Cerrar Session
+            </button>
+          </div>
 
-        <div id="disqus_thread" style={{ display: "none" }}></div>
-      </div>
+          <div id="disqus_thread" style={{ display: "none" }}></div>
+        </div>
+      </>
     )
   );
 };
