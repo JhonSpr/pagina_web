0a0c1b73bc01ed19df259653a209d8d32e1c1b9b
JhonSpr <jhoreyes20h@gmail.com>
2022-12-25 19:15:43 -0500

    a

--- src/Componentes/Elements/Filter.js
+++ src/Componentes/Elements/Filter.js
@@ -17,9 +17,7 @@ export class Filter extends React.Component {
   render() {
     return (
       <form action={"/" + this.state.value.toLowerCase().replace(/ /g, "-")}>
-        <h1
-          style={{ color: "#00f4f8" }}
-        >{`seleccionastes: ${this.state.value}`}</h1>
+        <h1 style={{ color: "#00f4f8" }}>{`Selección: ${this.state.value}`}</h1>
         <select
           value={this.state.value}
           onChange={this.handleChange}
