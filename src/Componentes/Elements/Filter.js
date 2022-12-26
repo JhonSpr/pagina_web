bdb25a10a2194e0729b0cdbd0f3498bc62c866a6
JhonSpr <jhoreyes20h@gmail.com>
2022-12-25 19:03:41 -0500

    a

--- src/Componentes/Elements/Filter.js
+++ src/Componentes/Elements/Filter.js
@@ -16,7 +16,11 @@ export class Filter extends React.Component {
 
   render() {
     return (
-      <form action={"/" + this.state.value.toLowerCase().replace(/ /g, "-")}>
+      <form
+        action={
+          "/" + this.state.value.toLowerCase().replace(/ /g, "-") + "?type="
+        }
+      >
         <h1>{`seleccionastes: ${this.state.value}`}</h1>
         <select
           value={this.state.value}
