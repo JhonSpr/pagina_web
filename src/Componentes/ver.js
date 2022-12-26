649a633c2e48c9781dc7f1f9d26076cbebf44816
JhonSpr <jhoreyes20h@gmail.com>
2022-12-26 02:11:02 -0500

    a

--- src/Componentes/ver.js
+++ src/Componentes/ver.js
@@ -7,19 +7,7 @@ export function Ver() {
       <Routes>
         <Route path="/ver" element={<h2>UPS NO SE ENCONTRO LA PAGINA</h2>} />
       </Routes>
-      <div className="container contenedor-NoFound">
-        <title>
-          Pagina No Encontrada <i class="fa-solid fa-triangle-exclamation"></i>{" "}
-        </title>
-        <h1 className="Title-NoFound">
-          <i class="fa-solid fa-triangle-exclamation"></i> Asegurate que el
-          anime buscado esta correctamente escrito{" "}
-          <i class="fa-solid fa-triangle-exclamation"></i>
-        </h1>
-        <a href="/" className="button is-orange">
-          Volver Al Inicio
-        </a>
-      </div>
+      .
     </>
   );
 }
