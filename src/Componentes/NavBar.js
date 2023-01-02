ed919b0965ba2ae9c4bfeb4106aa0163f4175ffb
JhonSpr <jhoreyes20h@gmail.com>
2023-01-02 17:27:24 -0500

    a

--- src/Componentes/NavBar.js
+++ src/Componentes/NavBar.js
@@ -102,7 +102,7 @@ export default function Menubar() {
                         <LogoutButton />
                       </>
                     ) : (
-                      <LoginButton />
+                      <></>
                     )}
                   </li>
                 </ul>
